c9280f481c26f04e7df91a162533d5fd6bbcaed0
corvis <corvis.mail@gmail.com>
2022-05-31 16:01:28 +0300

    PyPi build

--- src/hidlroute/cli.py
+++ src/hidlroute/cli.py
@@ -1,36 +1,42 @@
-#!/usr/bin/env python
-
 #    Hidl Route - opensource vpn management system
 #    Copyright (C) 2023 Dmitry Berezovsky, Alexander Cherednichenko
-#    
+#
 #    Hidl Route is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
-#    
+#
 #    Hidl Route is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
-#    
+#
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-try:
-    if os.environ.get("REMOTE_DEBUG", "0").lower() in ["yes", "true", "1"]:
-        import pydevd_pycharm
+WORKER_RELATED_COMMANDS = ("workers", "celery", "scheduler")
 
-        pydevd_pycharm.settrace('localhost', port=int(os.environ.get("REMOTE_DEBUG_PORT", "9543")),
-                                stdoutToServer=True, stderrToServer=True)
-except ImportError:
-    pass
 
-if __name__ == '__main__':
-    from hidlroute import cli as hidlroute_cli
+def entrypoint():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hidlroute.settings.prod")
+    for x in sys.argv:
+        if x.lower().strip() in WORKER_RELATED_COMMANDS:
+            os.environ.setdefault("HIDL_WORKERS", "true")
+            break
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hidlroute.settings.dev')
-    hidlroute_cli.entrypoint()
+
+if __name__ == "__main__":
+    entrypoint()
